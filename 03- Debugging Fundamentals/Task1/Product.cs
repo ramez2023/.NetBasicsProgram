9dc397d38e2267e31c222b83ea47c2e8574cbe0b
Ramez_Nady@epam.com <Ramez_Nady@epam.com>
2023-10-17 09:02:47 +0200

    03- Debugging Fundamentals

---
+++ 03- Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        private string Name { get; set; }
+
+        private double Price { get; set; }
+
+
+        public override bool Equals(object expected)
+        {
+            if (expected == null || GetType() != expected.GetType())
+                return false;
+
+            Product other = (Product)expected;
+            return Name == other.Name && Price == other.Price;
+        }
+    }
+}
---
+++ 03- Debugging Fundamentals/Task1/Utilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Sorts an array in ascending order using bubble sort.
+        /// </summary>
+        /// <param name="numbers">Numbers to sort.</param>
+        public static void Sort(int[] numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException(nameof(numbers));
+
+            var length = numbers.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i; j < length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        // feature introduced in C# 7.0 called "tuple deconstruction" or "tuple swapping."
+                        // It allows you to swap the values of two variables (or array elements, in this case) without using a temporary variable.
+                        (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// based on a <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="products">Products used for searching.</param>
+        /// <param name="predicate">Product predicate.</param>
+        /// <returns>If match found then returns index of product in <paramref name="products"/>
+        /// otherwise -1.</returns>
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            if (products == null)
+                throw new ArgumentNullException(nameof(products));
+
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (predicate(products[i]))
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
